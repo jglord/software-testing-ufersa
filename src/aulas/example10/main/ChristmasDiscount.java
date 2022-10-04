b67c54d8ff6a1c592228ca42871dd2fdccd3e2d7
joao goulart <joao123goulart@gmail.com>
2022-10-04 10:16:09 -0300

    04/10 -> Aula --> Init!

---
+++ src/aulas/example10/main/ChristmasDiscount.java
@@ -0,0 +1,25 @@
+package aulas.example10.main;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.Month;
+
+public class ChristmasDiscount {
+    private final Clock clock;
+
+    public ChristmasDiscount(Clock clock) {
+        this.clock = clock;
+    }
+
+    public double applyDiscount(double rawAmount) {
+        LocalDate today = LocalDate.now();
+
+        double discountPercentage = 0;
+        boolean isChristmas = today.getMonth() == Month.DECEMBER && today.getDayOfMonth() == 25;
+        if (isChristmas)
+            discountPercentage = 0.15;
+
+        return rawAmount - (rawAmount * discountPercentage);
+
+    }
+}
---
+++ src/aulas/example10/test/ChristmasDiscountTest.java
@@ -0,0 +1,17 @@
+package aulas.example10.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+
+import static org.mockito.Mockito.mock;
+
+public class ChristmasDiscountTest {
+    private final Clock clock = mock(Clock.class);
+
+    @Test
+    public void christmas() {
+        
+    }
+
+}
