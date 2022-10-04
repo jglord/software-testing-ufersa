1dd844e62661e614a3ece093d935653e6d85a892
joao goulart <joao123goulart@gmail.com>
2022-10-04 11:24:54 -0300

    04/10 -> Aula --> Ok!

--- src/aulas/example10/test/UserServiceTest.java
+++ src/aulas/example10/test/UserServiceTest.java
@@ -1,6 +1,9 @@
 package aulas.example10.test;
 
 import aulas.example10.main.PasswordEncoder;
+import aulas.example10.main.User;
+import aulas.example10.main.UserRepository;
+import aulas.example10.main.UserService;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import org.mockito.ArgumentCaptor;
@@ -21,6 +24,12 @@ public class UserServiceTest {
 
     String validPasswords[]     = {"@U332s", "k@C825", "*m6U41", "cX014$", "32@M5f", "R0j9%3"};
     String invalidPasswords[]   = {"abc", "", "12a", "123as", "asd21", "1"};
+    UserRepository ur = new UserRepository() {
+        @Override
+        public User findById(String id) {
+            return null;
+        }
+    };
 
     @Test // T1 -> Exercite senhas válidas de 6 caracteres (pesquise dicas para senhas fortes).
     void validPassword(){
@@ -48,14 +57,17 @@ public class UserServiceTest {
         }
     }
 
-
     @Test // T4 -> Verifique as interações com a interface PasswordEncoder para testar argumentos nas interações (Dica: org.mockito.ArgumentCaptor).
     void verifyArguments() {
+
+        UserService us = new UserService(ur, passwordEncoder);
         ArgumentCaptor<String> passwordCaptor = ArgumentCaptor.forClass(String.class);
-        for(String password : validPasswords){
-            passwordEncoder.encode(password);
+        for(int i = 0; i < 6; i++){
+            us.isValidUser(String.valueOf(i), validPasswords[i]);
+            //passwordEncoder.encode(validPasswords[i]);
+
             verify(passwordEncoder).encode(passwordCaptor.capture());
-            assertEquals(password, passwordCaptor.getValue());
+            assertEquals(validPasswords[i], passwordCaptor.getValue());
         }
     }
 }
