85bb67f6891fd8e54c135fd4e0f76e7338e79940
joao goulart <joao123goulart@gmail.com>
2022-10-06 10:57:07 -0300

    06/10 -> Aula --> OK!

--- src/aulas/example10/test/UserServiceTest.java
+++ src/aulas/example10/test/UserServiceTest.java
@@ -6,6 +6,9 @@ import aulas.example10.main.UserRepository;
 import aulas.example10.main.UserService;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.ArgumentCaptor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,51 +25,42 @@ public class UserServiceTest {
     //----------------------------------------------
     //----------------------------------------------
 
-    String validPasswords[]     = {"@U332s", "k@C825", "*m6U41", "cX014$", "32@M5f", "R0j9%3"};
-    String invalidPasswords[]   = {"abc", "", "12a", "123as", "asd21", "1"};
-    UserRepository ur = new UserRepository() {
-        @Override
-        public User findById(String id) {
-            return null;
-        }
-    };
-
-    @Test // T1 -> Exercite senhas válidas de 6 caracteres (pesquise dicas para senhas fortes).
-    void validPassword(){
-        for(String validPassword : validPasswords) {
-            when(passwordEncoder.encode(validPassword)).thenReturn("1");
-            passwordEncoder.encode(validPassword);
-            verify(passwordEncoder).encode(validPassword);
-        }
+    // T1 -> Exercite senhas válidas de 6 caracteres (pesquise dicas para senhas fortes).
+    @ParameterizedTest
+    @CsvSource({"@U332s", "k@C825", "*m6U41", "cX014$", "32@M5f", "R0j9%3"})
+    void validPassword(String validPassword){
+        when(passwordEncoder.encode(validPassword)).thenReturn("1");
+        passwordEncoder.encode(validPassword);
+        verify(passwordEncoder).encode(validPassword);
     }
 
-    @Test // T2 -> Exercite senhas inválidas.
-    void invalidPassword(){
-        for(String invalidPassword : invalidPasswords){
-            when(passwordEncoder.encode(invalidPassword)).thenReturn("0");
-            passwordEncoder.encode(invalidPassword);
-            verify(passwordEncoder).encode(invalidPassword);
-        }
+    // T2 -> Exercite senhas inválidas.
+    @ParameterizedTest
+    @CsvSource({ "abc", ".", "12a", "123as", "asd21", "1" })
+    void invalidPassword(String invalidPassword){
+        when(passwordEncoder.encode(invalidPassword)).thenReturn("0");
+        passwordEncoder.encode(invalidPassword);
+        verify(passwordEncoder).encode(invalidPassword);
     }
 
-    @Test // T3 -> Verifique as interações com a interface PasswordEncoder, para garantir que cada senha seja verificada uma vez (Dica: times()).
-    void verifyInteractions() {
-        for(String validPassword : validPasswords) {
-            passwordEncoder.encode(validPassword);
-            verify(passwordEncoder, times(1)).encode(validPassword);
-        }
+    // T3 -> Verifique as interações com a interface PasswordEncoder, para garantir que cada senha seja verificada uma vez (Dica: times()).
+    @ParameterizedTest
+    @CsvSource({ "@U332s", "k@C825", "*m6U41", "cX014$", "32@M5f", "R0j9%3" })
+    void verifyInteractions(String password) {
+        passwordEncoder.encode(password);
+        verify(passwordEncoder, times(1)).encode(password);
     }
 
-    @Test // T4 -> Verifique as interações com a interface PasswordEncoder para testar argumentos nas interações (Dica: org.mockito.ArgumentCaptor).
-    void verifyArguments() {
-
-        UserService us = new UserService(ur, passwordEncoder);
+    // T4 -> Verifique as interações com a interface PasswordEncoder para testar argumentos nas interações (Dica: org.mockito.ArgumentCaptor).
+    @ParameterizedTest
+    @CsvSource({ "@U332s", "k@C825", "*m6U41", "cX014$", "32@M5f", "R0j9%3" })
+    void verifyArguments(String password) {
         ArgumentCaptor<String> passwordCaptor = ArgumentCaptor.forClass(String.class);
-        for(int i = 0; i < 6; i++){
-            us.isValidUser(String.valueOf(i), validPasswords[i]);
-            //passwordEncoder.encode(validPasswords[i]);
-            verify(passwordEncoder).encode(passwordCaptor.capture());
-            assertEquals(validPasswords[i], passwordCaptor.getValue());
-        }
+        passwordEncoder.encode(password);
+        verify(passwordEncoder).encode(passwordCaptor.capture());
+
+        String generatedString = passwordCaptor.getValue();
+
+        assertThat(password).isEqualTo(generatedString);
     }
 }
